8081255465bd46892fb9730b308362a51550b643
Lasitha Weerasinghe <lasitha1991@hotmail.com>
2013-11-24 20:01:52 +0530

    exception

--- src/Logic/ISProject.java
+++ src/Logic/ISProject.java
@@ -13,7 +13,7 @@ public class ISProject {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // TODO code application logic here
         new GameEngine();
     }
